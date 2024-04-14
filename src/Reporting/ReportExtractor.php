723627b72e44329064999df53f877c5d2c98950a
Brice <brice.pote@gmail.com>
2024-04-14 14:04:16 +0200

    CHANGE ReportExtractor class
    - Add new public addFormaters method
    - Code refactoring on process method

--- src/Reporting/ReportExtractor.php
+++ src/Reporting/ReportExtractor.php
@@ -2,10 +2,7 @@
 
 namespace App\Reporting;
 
-use App\Reporting\Format\CsvFormatter;
-use App\Reporting\Format\HtmlFormatter;
-use App\Reporting\Format\HtmlSpecialFormatter;
-use App\Reporting\Format\JsonFormatter;
+use App\Reporting\Format\FormatterInterface;
 use JsonException;
 
 class ReportExtractor
@@ -13,32 +10,31 @@ class ReportExtractor
 	private string $title;
 	private string $date;
 	private array $data;
+	private array $formatters = [];
+
+
+	public function addFormatters(FormatterInterface $formatter):void
+	{
+		$this->formatters[] = $formatter;
+	}
 
 	/**
 	 * Doit afficher l'ensemble des formats possibles pour un rapport en se servant
 	 * des formatters ajoutés dans le tableau
 	 * @throws JsonException
 	 */
-    public function process(Report $report): array
+    public function process(Report $report, bool $withStringReport = false): array
     {
         $results = [];
 
-		//set the params for the StringReport
-		$this->setStringReportsParams($report);
-		$stringReport = new StringReport($this->title, $this->date, $this->data);
+		//add the formatters to the results
+	    foreach ($this->formatters as $formatter) {
+	        $results[] = $formatter->format($report);
+	    }
 
-        $htmlFormatter = new HtmlFormatter();
-        $htmlSpecialFormatter = new HtmlSpecialFormatter();
-        $jsonFormatter = new JsonFormatter();
-        $csvFormatter = new CsvFormatter();
-
-        $results[] = $htmlFormatter->format($report);
-        $results[] = $htmlSpecialFormatter->format($report);
-        $results[] = $jsonFormatter->format($report);
-        $results[] = $csvFormatter->format($report);
-
-		//Add the new StringReport to the results
-        $results[] = $stringReport->getStringReport();
+		if ($withStringReport) {
+			$results[] = $this->addStringReports($report);
+		}
 
         return $results;
     }
@@ -48,5 +44,17 @@ class ReportExtractor
 		$this->title = $report->getContents()['title'];
 		$this->date = $report->getContents()['date'];
 		$this->data = $report->getContents()['data'];
+	}
+
+	private function addStringReports(Report $report): array
+	{
+		$results = [];
+		//set the params for the StringReport
+		$this->setStringReportsParams($report);
+		$stringReport = new StringReport($this->title, $this->date, $this->data);
+
+		//Add the new StringReport to the results
+		$results[] = $stringReport->getStringReport();
+		return $results;
 	}
 }
